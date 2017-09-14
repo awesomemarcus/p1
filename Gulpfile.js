89d22942b7abe002976c93ce457ea70ea0ad11da
marcus <marcus@snapzio.com>
2017-09-14 08:09:21 +0800

    init setup gulp dependencies

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,7 +5,8 @@ var inject = require('gulp-inject');
 var wiredep = require('wiredep').stream;
 
 gulp.task('sass', function() {
-  var injectAppFiles = gulp.src('public/sass/*.scss', {read: false});
+  var injectAppFiles = gulp.src('public/sass/styles/*.scss', {read: false});
+  var injectGlobalFiles = gulp.src('public/sass/global/*scss', {read: false});
 
   function transformFilepath(filepath) {
     return '@import "' + filepath + '";';
@@ -18,18 +19,26 @@ gulp.task('sass', function() {
     addRootSlash: false
   };
 
-  gulp.src('./public/sass/main.scss')
+  var injectGlobalOptions = {
+    transform: transformFilepath,
+    starttag: '// inject:global',
+    endtag: '// endinject',
+    addRootSlash: false
+  };
+
+  return gulp.src('public/sass/main.scss')
       .pipe(wiredep())
+      .pipe(inject(injectGlobalFiles, injectGlobalOptions))
       .pipe(inject(injectAppFiles, injectAppOptions))
       .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
       .pipe(sourcemaps.write('./maps'))
-      .pipe(gulp.dest('./public/css'));
+      .pipe(gulp.dest('public/css'));
 });
 
 
 gulp.task('sass-watch', function() {
-  gulp.watch('./public/sass/**/*.scss', ['sass']);
+  gulp.watch(['./public/sass/**/**/*.scss', 'bower_components/bootstrap-sass/**/*.scss'], ['sass']);
 
   var injectFiles = gulp.src(['public/css/main.css']);
 
